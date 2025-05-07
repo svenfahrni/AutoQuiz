9dc2e36c92047670fc3bf20b07c855f58e9319e2
Sven Fahrni <sven.fahrni@fhnw.ch>
2025-05-07 18:32:15 +0200

    define interface

---
+++ Quizlet/Models/Card.cs
@@ -0,0 +1,25 @@
+using System.Text.Json.Serialization;
+
+namespace Quizlet.Models
+{
+    public class Card
+    {
+        [JsonPropertyName("front")]
+        public string Front { get; set; }
+
+        [JsonPropertyName("back")] 
+        public string Back { get; set; }
+
+        public Card()
+        {
+            Front = string.Empty;
+            Back = string.Empty;
+        }
+
+        public Card(string front, string back)
+        {
+            Front = front;
+            Back = back;
+        }
+    }
+}
---
+++ Quizlet/Models/CardDeck.cs
@@ -0,0 +1,33 @@
+using System.Text.Json.Serialization;
+using System.Collections.Generic;
+
+namespace Quizlet.Models
+{
+    public class CardDeck
+    {
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("cards")]
+        public List<Card> Cards { get; set; }
+
+        public CardDeck()
+        {
+            Title = string.Empty;
+            Cards = new List<Card>();
+        }
+
+        public CardDeck(string title)
+        {
+            Title = title;
+            Cards = new List<Card>();
+        }
+
+        public CardDeck(string title, List<Card> cards)
+        {
+            Title = title;
+            Cards = cards;
+        }
+
+    }
+} 
